cb3f6cbc2ff110422d0db5a55d68be3605e5bc88
Geovany <60295269+GeovanyBR2@users.noreply.github.com>
2024-11-05 22:08:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tecnologia',
+    'O que é inteligência artificial?',
+    'Inteligência artificial é a simulação de processos de inteligência humana por máquinas, como aprendizado e raciocínio'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é a computação em nuvem?',
+    'Computação em nuvem é o uso de servidores remotos para armazenar, gerenciar e processar dados pela internet'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência de instruções para realizar uma tarefa ou resolver um problema'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'What is the capital of the United Kingdom?',
+    'The capital of the United Kingdom is London'
+)
